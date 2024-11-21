6a70bdfc11392a327add0ff9b06378edec627601
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-11-21 17:20:55 +0200

    Array Pop Method

---
+++ Arrays/pop.js
@@ -0,0 +1,7 @@
+// The Pop method of an array removes the last element of an array and return that element that was removed
+
+const randomNumbers = [2,3,5,1,5,1,6,2,8,2,100]
+// this would remove the last element from the array
+const updatedArray =   randomNumbers.pop()
+//this will return the element that was removed 
+console.log(updatedArray)
