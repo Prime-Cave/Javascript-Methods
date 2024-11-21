aa0a725f53c57c015bad2a6f58eab69c46c41d01
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-11-21 17:09:55 +0200

    Initial commit & Push Method on Arrays

---
+++ Arrays/push.js
@@ -0,0 +1,7 @@
+// the push method of an array adds a new element to the end of an array and returns the new length of the
+
+const randomNumbers = [2,3,5,1,5,1,6,2,8,2,0]
+
+const updatedArray = randomNumbers.push(2)
+// The Push Method would return the length of the arracy after it is updated.
+console.log(updatedArray)
