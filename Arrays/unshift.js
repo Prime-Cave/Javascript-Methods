f570de6572a760c36201530a7374230a282a8d4e
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-11-22 14:40:39 +0200

    unshifts removes the first element and returns the length of the updated array

---
+++ Arrays/unshift.js
@@ -0,0 +1,9 @@
+// The unshift() method of Array instances adds the specified elements to the beginning of an array and returns the new length of the array.
+
+const randomNumbers = [2,3,5,1,5,1,6,2,8,2,100]
+// this would add to the first element of the array
+const updatedArray =   randomNumbers.unshift(13,14)
+//this will return the length of the array after it has been updated
+console.log(updatedArray)
+// After the first element has been removed 
+console.log(randomNumbers)
