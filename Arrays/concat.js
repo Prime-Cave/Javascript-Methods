33f53dbae2752b112699ed6c26b970223fe0f229
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-11-26 16:23:41 +0200

    array concat

---
+++ Arrays/concat.js
@@ -0,0 +1,9 @@
+// The concat() method of Array instances is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+
+const array1 = [1,2,3,4]
+const array2 = [5,6,7,8]
+
+// concat(value1, value2)
+const array3 = array1.concat(array2)
+
+console.log(array3)
