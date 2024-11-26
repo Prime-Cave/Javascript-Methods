bd874b5c96fd32ea091041dbc62ac139c66655b4
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-11-26 16:30:05 +0200

    indexOf

---
+++ Arrays/indexOf.js
@@ -0,0 +1,7 @@
+//The concat() method of Array instances is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+// indexOf(searchElement)
+// indexOf(Element-being-searched-for , From-what-index-to-start-searching-from)
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('ant',2))
