33d90ddb3c3da39736fe5d9171241dc092168dd7
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2024-12-13 08:53:48 +0200

    Some changes

--- Arrays/indexOf.js
+++ Arrays/indexOf.js
@@ -1,4 +1,3 @@
-//The concat() method of Array instances is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
 // indexOf(searchElement)
 // indexOf(Element-being-searched-for , From-what-index-to-start-searching-from)
 
--- Arrays/push.js
+++ Arrays/push.js
@@ -4,5 +4,5 @@ const randomNumbers = [2,3,5,1,5,1,6,2,8,2,0]
 //push(element1, element2)
 // Takes as many elements 
 const updatedArray = randomNumbers.push(2)
-// The Push Method would return the length of the arracy after it is updated.
+// The Push Method would return the length of the array after it is updated.
 console.log(updatedArray)
